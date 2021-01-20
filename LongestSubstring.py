1093bda54c944ba2dceea4674289eebd114f5eea
Nicht-menschlich <j.35ul.54@gmail.com>
2021-01-20 09:23:40 +0100

    PyCharm Upload

---
+++ LongestSubstring.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        strList = list(s)
+        maxLen = 0
+        for i in range(len(s)):
+            subStr = ""
+            for c in range(i, len(s)):
+                curChar = strList[c]
+                if curChar not in subStr:
+                    subStr += curChar
+                else:
+                    break
+            if len(subStr) > maxLen:
+                maxLen = len(subStr)
+        return maxLen
