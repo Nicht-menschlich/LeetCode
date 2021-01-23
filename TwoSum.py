daf712083e1c42679823b49882e115e167e3c0dc
Nicht-menschlich <j.35ul.54@gmail.com>
2021-01-23 17:02:23 +0100

    Finished twoSum

---
+++ TwoSum.py
@@ -0,0 +1,9 @@
+from typing import List
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(len(nums)):
+                if nums[i] + nums[j] == target and i is not j:
+                    return [i, j]
