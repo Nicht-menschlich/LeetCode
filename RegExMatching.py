fd849833b2c2319fc1f7328a204d70a115cf1f0d
Nicht-menschlich <j.35ul.54@gmail.com>
2023-01-24 11:36:33 +0100

    Revert "PalindromeNumber.py"
    This reverts commit 82ccd4971b9f62d059433630d7c35eb24850565d.

--- RegExMatching.py
+++
@@ -1,8 +0,0 @@
-#https://leetcode.com/problems/regular-expression-matching/
-import re
-class Solution:
-    def isMatch(self, s: str, p: str) -> bool:
-        match = re.match(p, s)
-        if not match:
-            return False
-        return match.group() == s
